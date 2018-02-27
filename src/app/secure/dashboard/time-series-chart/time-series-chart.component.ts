3910cc79966e7abe6718cb4cb77ee38c443fadd2
Shyam Kannan <csystic@gmail.com>
2018-02-27 14:13:02 +0530

    ff_v7

--- src/app/secure/dashboard/time-series-chart/time-series-chart.component.ts
+++ src/app/secure/dashboard/time-series-chart/time-series-chart.component.ts
@@ -9,13 +9,17 @@ import { StoresService } from '../../../services/stores.service';
 })
 export class TimeSeriesChartComponent implements OnInit {
 
+  barsPerSlide = 7;
   timeSeriesData: any;
   selectedDate: any;
+  slideIndexArray: Array<number> = [];
 
   // i am hardcoding the scales right now but this will need to be
   // updated on some logic which i am not clear as of now
   activeScale = 12;
   issueScale = 8;
+  currentHour: any;
+  activeSlideIndex: number;
 
   constructor(private storesService: StoresService) { }
 
@@ -32,6 +36,16 @@ export class TimeSeriesChartComponent implements OnInit {
       'month': currentDate.getMonth() + 1,
       'day': currentDate.getDate()
     };
+
+    // populate slideIndex array
+    const numOfSlides = Math.ceil(24 / this.barsPerSlide);
+    for (let i = 0; i < numOfSlides; i++) {
+      this.slideIndexArray.push(i);
+    }
+
+    // get active slide index based on current time
+    this.currentHour = currentDate.getHours();
+    this.activeSlideIndex = Math.floor(this.currentHour / this.barsPerSlide);
   }
 
   formatDate() {
